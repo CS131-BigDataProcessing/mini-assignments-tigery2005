4e19337975c2b88a9a14f284947a23923fddc477
Tiger <tigeryang2005@gmail.com>
2024-09-11 09:54:52 -0700

    A4_basic-shell (updated)

--- Yang-T_A4/echoing.sh
+++ Yang-T_A4/echoing.sh
@@ -21,7 +21,7 @@ mathvar1=$[1+5]
 mathvar2=$[mathvar1*20]
 mathvar3=10
 mathvar4=$[mathvar1*(mathvar2+mathvar3)]
-echo "Variable 1 is $mathvar1. Variable 2 is $mathvar2. Variable 3 is $mathvar3. Variable 4 is $mathvar4.\n"
+echo "Variable 1 is $mathvar1. Variable 2 is $mathvar2. Using $mathvar3 for Variable 3, our final Variable 4 is $mathvar4.\n"
 
 # Activity 4
 
